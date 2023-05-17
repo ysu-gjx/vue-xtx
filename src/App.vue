d4e7fa32f8130e5f2026e2325d3fdf3294d032fe
gjx <252932110@qq.com>
2023-05-17 20:23:54 +0800

    feat: 引入图标

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <script setup>
-import { ref, watch, onMounted, provide } from 'vue'
-import { useCounterStore } from './stores/counter'
-import { storeToRefs } from 'pinia'
-import { getCategoryAPI } from '@/apis/testAPI'
-
-
-const counterStore = useCounterStore()
-const { count, doubleCount } = storeToRefs(counterStore)
-const { increment } = counterStore
-const size = 'small'
-const zIndex = 3000
-
-onMounted(() => {
-  counterStore.getList()
-  getCategoryAPI()
-})
+import Layout from '@/views/Layout/index.vue';
 
 </script>
 
 <template>
-  <el-config-provider :size="size" :z-index="zIndex">
-    <el-button type="primary">Primary</el-button>
-    <div>
-      <button @click="increment">{{ count }}</button>
-      <span>{{ doubleCount }}</span>
-      <p>
-        <router-link to="/">Go to Home</router-link>
-        <router-link to="/login">Go to Login</router-link>
-      </p>
-      <router-view></router-view>
-    </div>
-  </el-config-provider>
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.test {
+  color: $priceColor
+}
+</style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,11 +1,14 @@
 <script setup>
-
+import LayoutFooter from './components/LayoutFooter.vue';
+import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutNav from './components/LayoutNav.vue';
 </script>
 <template>
-  <div>这是布局页
+  <div>
+    <layout-nav />
+    <LayoutHeader />
     <router-view></router-view>
+    <LayoutFooter />
   </div>
 </template>
-<style lang="" scoped>
-
-</style>
+<style lang="scss" scoped></style>
